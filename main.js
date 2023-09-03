5d7c8669638bdac6047b3843cd2701cb638309bb
Rakshak kumar <kingrakshak199@gmail.com>
2023-09-04 01:32:49 +0530

    Added Beatboxx files

---
+++ main.js
@@ -0,0 +1,18 @@
+// Example JavaScript function
+function playSong(songId) {
+    // Implement your logic here
+    console.log("Playing song with ID:", songId);
+  }
+  
+  // JavaScript code for handling the search functionality
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  
+  searchButton.addEventListener("click", performSearch);
+  
+  function performSearch() {
+    const searchTerm = searchInput.value.trim();
+    // Perform the search operation based on the searchTerm
+    // Update the UI with the search results
+  }
+  
